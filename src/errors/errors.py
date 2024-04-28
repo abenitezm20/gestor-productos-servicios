33520b94b6faf41cb8bc5e99f15c19e1d130c8cc
AlexB <a.benitezm@uniandes.edu.co>
2024-04-27 21:09:08 -0500

    Carga Inicial

---
+++ src/errors/errors.py
@@ -0,0 +1,38 @@
+class ApiError(Exception):
+    code = 500
+    description = "internal_server_error"
+
+
+class BadRequest(ApiError):
+    code = 400
+    description = "bad_request"
+
+
+class Unauthorized(ApiError):
+    code = 401
+    description = "unauthorized"
+
+
+class Forbidden(ApiError):
+    code = 403
+    description = "forbidden"
+
+
+class TokenNotFound(ApiError):
+    code = 403
+    description = "token_not_found"
+
+
+class ResourceNotFound(ApiError):
+    code = 404
+    description = "resource_not_found"
+
+
+class PreconditionFailed(ApiError):
+    code = 412
+    description = "precondition_failed"
+
+
+class UserAlreadyExist(ApiError):
+    code = 432
+    description = "user_already_exists"
---
+++ src/models/model.py
@@ -0,0 +1,14 @@
+import uuid
+from datetime import datetime
+from sqlalchemy import Column, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class Model():
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    def __init__(self):
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
