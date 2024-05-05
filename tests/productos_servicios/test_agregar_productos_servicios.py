035a485179f9e3eb1e5783c225bc48abf1b2387d
AlexB <a.benitezm@uniandes.edu.co>
2024-05-05 17:02:37 -0500

    Test Ajuste Consulta por ID

--- tests/productos_servicios/test_agregar_productos_servicios.py
+++ tests/productos_servicios/test_agregar_productos_servicios.py
@@ -301,4 +301,70 @@ class TestProductosServicios():
                     delSocioNegocio = delete(SocioNegocio).where(SocioNegocio.id == socio_id)
                     session.execute(delSocioNegocio)
                     session.commit()
+    
+
+    @patch('requests.post')
+    def test_listar_productos_servicios_porID(self, mock_post, setup_data: SocioNegocio):
+        with db_session() as session:
+            with app.test_client() as test_client:
+
+                # Crear Socio de Negocio
+                socio = {
+                    'nombre':fake.name(),
+                    'tipo_identificacion':fake.random_element(elements=(
+                        tipo_identificacion.value for tipo_identificacion in TipoIdentificacionSocioEnum)),
+                    'numero_identificacion':fake.random_int(min=1000000, max=999999999),
+                    'email':fake.email(),
+                    'contrasena':fake.password()
+                }
+        
+                socio_random: SocioNegocio = SocioNegocio(**socio)
+                session.add(socio_random)
+                session.commit()
+                socio_email = socio_random.email
+                socio_id= socio_random.id
+
+                mock_response = MagicMock()
+                mock_response.status_code = 200
+                mock_response.json.return_value = {
+                    'token_valido': True, 
+                    'email': socio_random.email,
+                    'tipo_usuario': 'socio_negocio'
+                    }
+                mock_post.return_value = mock_response
+
+                headers = {'Authorization': 'Bearer 123'}
+
+                #Se agrega un producto
+                servicio_producto = {
+                    "email": socio_random.email,
+                    "deporte": 'Atletismo',
+                    "tipo": fake.random_element(elements=('producto', 'servicio')),
+                    "descripcion": fake.text(),
+                    "subtipo": fake.word(),
+                    "pais": fake.country(),
+                    "ciudad": fake.city(),
+                    "lugar_entrega_prestacion": fake.city(),
+                    "cantidad_disponible": fake.random_int(min=2, max=10),
+                    "fecha_entrega_prestacion": "2024-05-01T12:00:0",
+                    "valor": fake.random_int(min=10000, max=1000000)
+                }
+                response = test_client.post('/gestor-productos-servicios/productos-servicios/agregar', headers=headers, json=servicio_producto, follow_redirects=True)
+                response_json = json.loads(response.data)
+
+                tmp_socio = session.query(SocioNegocio).filter(SocioNegocio.email == socio_email).first()
+
+                if tmp_socio is not None:                 
+                    #Se hace la consulta de los productos
+                    responseConsulta = test_client.get('/gestor-productos-servicios/productos-servicios/listarID/'+response_json['id_servicio_producto'], headers=headers, follow_redirects=True)
+
+                    assert responseConsulta.status_code == 200
+
+                    delServicioProducto = delete(ServicioProducto).where(ServicioProducto.id_socio_negocio == socio_id)
+                    session.execute(delServicioProducto)
+                    session.commit()
+
+                    delSocioNegocio = delete(SocioNegocio).where(SocioNegocio.id == socio_id)
+                    session.execute(delSocioNegocio)
+                    session.commit()
                 
